153be902bada759af84e53821fb61f74f028e1d5
Taylor Smith <tsmith@tsmithcreative.com>
2017-02-24 15:21:53 +0000

    More theming ajdustments to projects and the archive

--- _plugins/tsc.rb
+++ _plugins/tsc.rb
@@ -36,8 +36,17 @@ module Jekyll
       end
     end
   end
+
+  # Projects contain a writeup and a bullet-list of some basic details.
+  # It's all in the body area divided by a horizontal rule.
+  module SeparateHorizRules
+    def fix_hr(input)
+      input.sub(/<hr \/>/, '</div><div class="project-details">')
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::MediaWrapper)
 Liquid::Template.register_filter(Jekyll::MastheadGenerate)
 Liquid::Template.register_filter(Jekyll::ThumbnailGenerate)
+Liquid::Template.register_filter(Jekyll::SeparateHorizRules)
