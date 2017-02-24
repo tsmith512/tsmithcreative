2ec3b70a6783f3ec23f593edbe4969878fca336c
Taylor Smith <tsmith@tsmithcreative.com>
2017-02-24 17:27:20 +0000

    Pull in tsmith512/tsmithphotos 's implementation of Photoswipe and hook it up to projects

--- gulpfile.js
+++ gulpfile.js
@@ -51,6 +51,7 @@ const uglify = require('gulp-uglify');
 
 */
 
+// CSS
 gulp.task('sass', 'Compile Sass to CSS', () => {
   return gulp.src('./_sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -69,21 +70,48 @@ gulp.task('sass', 'Compile Sass to CSS', () => {
     .pipe(gulp.dest('./_site/css'));
 });
 
+
+// IMAGES
 gulp.task('icons', false, gulpicon(gulpiconFiles, gulpiconConfig));
-
-gulp.task('graphics', 'Compress site graphics and aggregate icons', ['icons', 'graphics-project-thumbnails'], () => {
-  return gulp.src('./_gfx/**/*.*')
-    // .pipe(imagemin())
-    .pipe(gulp.dest('./_site/gfx/'));
-});
-
 
 gulp.task('graphics-project-thumbnails', 'Rebuild gallery thumbnails for project images', () => {
   return gulp.src('assets/projects/**/*.*')
-    .pipe(resize({width: 250, height: 250, crop: true, upscale: false}))
+    .pipe(resize({width: 200, height: 200, crop: true, upscale: false}))
     .pipe(imagemin([imagemin.jpegtran({progressive: true})]))
     .pipe(gulp.dest('_site/gfx/thumbs/projects/'))
 });
+
+gulp.task('graphics', 'Compress site graphics and aggregate icons', ['icons', 'graphics-project-thumbnails'], () => {
+  return gulp.src('./_gfx/**/*.*')
+  // .pipe(imagemin())
+  .pipe(gulp.dest('./_site/gfx/'));
+});
+
+
+// JS
+gulp.task('js-photoswipe', false, () => {
+  return gulp.src(['./node_modules/photoswipe/dist/*.js', '_js/photoswipe.tsp.js'])
+    .pipe(concat('photoswipe.all.js'))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('./_site/js'));
+});
+
+gulp.task('js-photoswipe-assets', false, () => {
+  return gulp.src(['./node_modules/photoswipe/dist/default-skin/*.png', './node_modules/photoswipe/dist/default-skin/*.svg', './node_modules/photoswipe/dist/default-skin/*.gif'])
+    .pipe(gulp.dest('./_site/css'));
+});
+
+gulp.task('js-all', false, () => {
+  return gulp.src([
+    './node_modules/fg-loadcss/src/loadCSS.js',
+    './node_modules/fg-loadcss/src/cssrelpreload.js'
+  ])
+    .pipe(concat('all.js'))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('./_site/js'));
+});
+
+gulp.task('js', 'JS/Photoswipe aggregation/minify, custom JS linting', ['js-photoswipe', 'js-photoswipe-assets', 'js-all']);
 
 /*
      _ _         _           _ _     _
@@ -103,7 +131,7 @@ gulp.task('jekyll', 'Run jekyll build', (cb) => {
 });
 
 gulp.task('build', 'Run all site-generating tasks: sass, js, graphics, icons, htaccess then jekyll', (cb) => {
-  runSequence(['sass', 'graphics', 'icons'], 'jekyll', cb);
+  runSequence(['sass', 'graphics', 'icons', 'js'], 'jekyll', cb);
 });
 
 /*
@@ -120,6 +148,7 @@ gulp.task('default', false, ['help']);
 gulp.task('watch', 'Watch-run sass, jekyll, js, graphics, and icons tasks', () => {
   gulp.watch(['./_sass/**/*.scss'], ['sass']);
   gulp.watch(['./*.*', './assets/**/*.*', './**/*.html', './**/*.yml', './**/*.markdown', './**/*.md', './**/*.rb', '!./node_modules/**', '!./_site/**'], ['jekyll']);
+  gulp.watch(['./**/*.js', '!./_site/**', '!./node_modules/**'], ['js']);
   gulp.watch(['./_gfx/**/*.*', './assets/**/*.*'], ['graphics']);
   gulp.watch(['./_icons/**/*.*'], ['icons']);
 });
