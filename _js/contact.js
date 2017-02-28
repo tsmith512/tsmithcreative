e74b912975beecbc939d5413a817deed365090b1
Taylor Smith <tsmith@tsmithcreative.com>
2017-02-28 06:40:15 +0000

    Let's be a little more diplomatic with that, shall we?

--- _js/contact.js
+++ _js/contact.js
@@ -21,7 +21,8 @@
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = function() {
       if (xhr.status === 200) {
-        alert('oh shit it worked');
+        document.querySelectorAll('.form-wrapper')[0].innerHTML =
+          '<h2>Thank you!</h2><p>Your email has been sent, I\'ll get back to you as soon as I can.';
       }
     };
     xhr.send(JSON.stringify(message));
