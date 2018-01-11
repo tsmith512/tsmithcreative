a1c598e6ecc23fda74fb12ea3bbfdc9f41674211
Taylor Smith <tsmith@tsmithcreative.com>
2018-01-11 21:44:07 +0000

    Exit the exit-on-error mode for NVM install and setup per (see nvm issue 1484)

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -7,6 +7,8 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 
 echo LOAD NVM
 export NVM_DIR="$HOME/.nvm"
+# nvm doesn't work right with `set -e` mode
+set +e
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
 cd ~/repo
@@ -16,6 +18,9 @@ nvm install $(cat ~/repo/.nvmrc)
 
 echo LOAD NODE VERSION
 nvm use
+
+# With nvm out of the way, go back into exit-on-error mode
+set -e
 
 echo INSTALL NODE DEPENDENCIES
 npm install -g gulp
