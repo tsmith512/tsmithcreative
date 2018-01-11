c50973ddf826f93080eb3e2f35edd9efcd76311e
Taylor Smith <tsmith@tsmithcreative.com>
2018-01-11 21:15:18 +0000

    Updates to the build script to be in the right directory

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,13 +9,16 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
+# Enter the repo directory
+cd ~/repo
+
+# Get Node
 nvm install
 nvm use
 
+# Get Node Dependencies
 npm install -g gulp
 npm install
 
-gulp assets
-gulp build
-
+# Build and deploy to the staging bucket
 gulp publish-staging
