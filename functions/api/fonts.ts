fbeff1280655f34a314eaa62eaacc04a629a9986
Taylor Smith <tsmith@tsmithcreative.com>
2022-11-12 13:23:40 -0600

    Put a basic proxy for typography.com to resolve the redirect server-side on-domain

---
+++ functions/api/fonts.ts
@@ -0,0 +1,33 @@
+const HFJ_BASE = 'https://cloud.typography.com/6795652/7424152/css/fonts.css';
+
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  const outboundHeaders = {
+    accept: 'text/css,*/*;q=0.1',
+    'accept-encoding': 'gzip, deflate, br',
+    'cache-control': 'no-cache',
+    host: 'cloud.typography.com',
+    pragma: 'no-cache',
+    referer: 'https://tsmith.com/',
+    'user-agent': request.headers.get('user-agent'),
+  };
+
+  const payload = await fetch(HFJ_BASE, {
+    headers: outboundHeaders,
+  });
+
+  // fetch() will follow the redirect code from typography.com and the payload
+  // will contain the self-hosted, user-agent-dependent CSS response.
+
+  return new Response(await payload.text());
+}
