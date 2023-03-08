00ca0f38ba8d60751abf108215056fcc8e75d9ff
Taylor Smith <tsmith@cloudflare.com>
2023-03-08 12:03:03 -0600

    Update the form submission handler to be a little less stone-agey.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,7 @@
 (function(){
   'use strict';
 
-  var form = document.getElementById("contact-form");
-
-  if (form) {
-    form.addEventListener("submit", emailHandler);
-  }
-
+  // Use a Cloudflare Turnstile challenge to unlock the Submit button
   window.turnstileInit = () => {
     console.log('turnstile init called');
     turnstile.render('#turnstileChallenge', {
@@ -19,10 +14,12 @@
     });
   };
 
-  function emailHandler(e) {
+  // Submission handler
+  const emailHandler = async (e) => {
     e.preventDefault();
 
     const cfEl = document.querySelector('input[name="cf-turnstile-response"]');
+    const formWrapper = document.querySelector('.form-wrapper');
 
     if (!cfEl) {
       // @TODO: Fallback? This element won't exist if Turnstile didn't init
@@ -36,23 +33,31 @@
       turnstile: (cfEl) ? cfEl.value : false,
     };
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', form.getAttribute("action"));
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        document.querySelector('.form-wrapper').innerHTML =
+    await fetch(form.getAttribute('action'), {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(message),
+    }).then((response) => {
+      if (response.status === 200) {
+        formWrapper.innerHTML =
           '<h2>Thank you!</h2><p>Your email has been sent, I\'ll get back to you as soon as I can.</p>';
       }
       else {
-        document.querySelector('.form-wrapper').innerHTML =
+        formWrapper.innerHTML =
           '<h2>Sorry.</h2><p>An error occurred sending this email. Would you mind trying one of the social media links instead?</p>';
       }
-    };
-    xhr.onerror = function() {
-      document.querySelector('.form-wrapper').innerHTML =
+    }).catch((error) => {
+      console.log(error);
+
+      formWrapper.innerHTML =
         '<h2>Sorry.</h2><p>An error occurred sending this email. Would you mind trying one of the social media links instead?</p>';
-    };
-    xhr.send(JSON.stringify(message));
+    });
   }
+
+  var form = document.getElementById("contact-form");
+
+  if (form) {
+    form.addEventListener("submit", emailHandler);
+  }
+
 })();
