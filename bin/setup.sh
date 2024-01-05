9717d67c9012f85ee33d4126ff2073ca9489f76e
Taylor Smith <tsmith@tsmithcreative.com>
2024-01-04 22:54:24 -0600

    Add Mermaid.js by downloading it to static and importing the module when needed

--- bin/setup.sh
+++ bin/setup.sh
@@ -15,6 +15,13 @@ curl $WEBFONTS \
 
 unzip -o -d public/fonts webfonts.zip
 
+# Unlike Photoswipe, the repo doesn't include a dist directory, and using the
+# ESM instead of the full script reduces download a lot, so we have to do this
+# in a weird way.
+mkdir -p static/vendor/mermaid
+curl https://registry.npmjs.org/mermaid/-/mermaid-10.6.1.tgz -o mermaid.tgz
+tar -zxf mermaid.tgz -C static/vendor/mermaid
+
 hugo
 
 # @TODO: This may not be the best way, but make sure modules are installed for
